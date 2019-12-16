1ca00fe281075669e141726bc7adc6be534e3228
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-12-15 23:23:24 -0500

    Added Watch ETH contract page and refactored Deploy ETH contract

--- service/views.py
+++ service/views.py
@@ -3,6 +3,9 @@ import json
 from decouple import config
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.defaultfilters import linebreaksbr
+from django.utils.html import linebreaks
+
 from console_main.views import login_required
 from django.contrib import messages
 from django.urls import reverse
@@ -14,7 +17,7 @@ from elastos_adenine.wallet import Wallet
 
 from .forms import UploadAndSignForm, VerifyAndShowForm
 from .forms import CreateWalletForm, ViewWalletForm, RequestELAForm
-from .forms import DeployETHContractForm
+from .forms import DeployETHContractForm, WatchETHContractForm
 from .models import UploadFile
 
 
@@ -103,27 +106,29 @@ def verify_and_show(request):
 @login_required
 def create_wallet(request):
     if request.method == "POST":
-        try:
-            wallet = Wallet()
-            api_key = request.POST['api_key']
-            eth_password = request.POST['eth_password']
-            response = wallet.create_wallet(api_key, eth_password)
-            if response.status:
-                content = json.loads(response.output)['result']
-                wallet_mainchain = content['mainchain']
-                wallet_did = content['sidechain']['did']
-                wallet_token = content['sidechain']['token']
-                wallet_eth = content['sidechain']['eth']
-                return render(request, "service/create_wallet.html", { 'output': True, 'wallet_mainchain': wallet_mainchain,
-                    'wallet_did': wallet_did, 'wallet_token': wallet_token, 'wallet_eth': wallet_eth })
-            else:
+        form = CreateWalletForm(request.POST)
+        if form.is_valid():
+            api_key = form.cleaned_data.get('api_key')
+            eth_password = form.cleaned_data.get('eth_password')
+            try:
+                wallet = Wallet()
+                response = wallet.create_wallet(api_key, eth_password)
+                if response.status:
+                    content = json.loads(response.output)['result']
+                    wallet_mainchain = content['mainchain']
+                    wallet_did = content['sidechain']['did']
+                    wallet_token = content['sidechain']['token']
+                    wallet_eth = content['sidechain']['eth']
+                    return render(request, "service/create_wallet.html", { 'output': True, 'wallet_mainchain': wallet_mainchain,
+                        'wallet_did': wallet_did, 'wallet_token': wallet_token, 'wallet_eth': wallet_eth })
+                else:
+                    messages.success(request, "Could not create wallet at this time. Please try again")
+                    return redirect(reverse('service:create_wallet'))
+            except Exception as e:
                 messages.success(request, "Could not create wallet at this time. Please try again")
                 return redirect(reverse('service:create_wallet'))
-        except Exception as e:
-            messages.success(request, "Could not create wallet at this time. Please try again")
-            return redirect(reverse('service:create_wallet'))
-        finally:
-            wallet.close()
+            finally:
+                wallet.close()
     else:
         form = CreateWalletForm()
         return render(request, 'service/create_wallet.html', {'output': False, 'form': form})
@@ -274,18 +279,26 @@ def deploy_eth_contract(request):
             form.save()
             temp_file = UploadFile.objects.get(did=did)
             file_path = temp_file.uploaded_file.path
-            console = SidechainEth()
-            response = console.deploy_eth_contract(api_key, eth_account_address, eth_account_password, file_path)
-            data = json.loads(response.output)
-            if response.status:
-                temp_file.delete()
-                contract_address = data['result']['contract_address']
-                return render(request, "service/deploy_eth_contract.html",
-                              {"contract_address": contract_address, 'output': True})
-            else:
-                messages.success(request, "File could not be uploaded. Please try again")
+            try:
+                sidechain_eth = SidechainEth()
+                response = sidechain_eth.deploy_eth_contract(api_key, eth_account_address, eth_account_password, file_path)
+                data = json.loads(response.output)
+                if response.status:
+                    temp_file.delete()
+                    contract_address = data['result']['contract_address']
+                    contract_name = data['result']['contract_name']
+                    contract_code_hash = data['result']['contract_code_hash']
+                    return render(request, "service/deploy_eth_contract.html",
+                                  {"contract_address": contract_address, "contract_name": contract_name,
+                                   "contract_code_hash": contract_code_hash, 'output': True})
+                else:
+                    messages.success(request, "Could not deploy smart contract to Eth sidechain. Please try again")
+                    return redirect(reverse('service:deploy_eth_contract'))
+            except Exception as e:
+                messages.success(request, "Could not deploy smart contract to Eth sidechain. Please try again")
                 return redirect(reverse('service:deploy_eth_contract'))
-
+            finally:
+                sidechain_eth.close()
     else:
         form = DeployETHContractForm(initial={'did': did})
         return render(request, "service/deploy_eth_contract.html", {'form': form, 'output': False})
@@ -293,7 +306,34 @@ def deploy_eth_contract(request):
 
 @login_required
 def watch_eth_contract(request):
-    return render(request, "service/watch_eth_contract.html")
+    if request.method == 'POST':
+        form = WatchETHContractForm(request.POST)
+        if form.is_valid():
+            api_key = form.cleaned_data.get('api_key')
+            contract_address = form.cleaned_data.get('contract_address')
+            contract_name = form.cleaned_data.get('contract_name')
+            contract_code_hash = form.cleaned_data.get('contract_code_hash')
+            try:
+                sidechain_eth = SidechainEth()
+                response = sidechain_eth.watch_eth_contract(api_key, contract_address, contract_name, contract_code_hash)
+                data = json.loads(response.output)
+                if response.status:
+                    contract_address = data['result']['contract_address']
+                    contract_functions = data['result']['contract_functions']
+                    contract_source = data['result']['contract_source']
+                    return render(request, "service/watch_eth_contract.html", {'output': True, 'contract_address': contract_address, 'contract_name': contract_name,
+                                                                               'contract_functions': contract_functions, 'contract_source': contract_source})
+                else:
+                    messages.success(request, "Could not view smart contract code at this time. Please try again")
+                    return redirect(reverse('service:watch_eth_contract'))
+            except Exception as e:
+                messages.success(request, "Could not view smart contract code at this time. Please try again")
+                return redirect(reverse('service:watch_eth_contract'))
+            finally:
+                sidechain_eth.close()
+    else:
+        form = WatchETHContractForm()
+        return render(request, 'service/watch_eth_contract.html', {'output': False, 'form': form})
 
 
 @login_required
