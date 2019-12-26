496f7c0c5dc1dbd5c62bf511aeb66493f1742203
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-12-26 17:59:49 -0500

    Fixing static directory

--- console_main/settings.py
+++ console_main/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
@@ -180,7 +180,7 @@ STATICFILES_FINDERS = [
 ]
 
 # Django Sass
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, "www", "static")
 SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r'^.+\.scss$'
 
 MEDIA_URL = '/media/'
