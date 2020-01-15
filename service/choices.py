25d506245375a1bba590019be94310a752104229
Ashwin_Pokharel <pokharel.ashwin1@gmail.com>
2020-01-15 17:53:48 -0500

    changed javascript code and updated migrations scripts

--- service/choices.py
+++ service/choices.py
@@ -6,3 +6,11 @@ NETWORK = (
 NETWORK_GMU = (
     ('gmunet', "GMUnet"),
 )
+
+CATEGORY = (
+    ('common', "Common"),
+    ('Hive' , 'Elastos Hive'),
+    ('eth_sidechain', "ETH Sidechain"),
+    ('wallet', "Wallet"),
+    ('playground' , "Playground"),
+)
