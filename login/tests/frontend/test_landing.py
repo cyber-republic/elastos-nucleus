290d9210208a3038b778d5a1b6b181ef9e90c93c
Ashwin_Pokharel <pokharel.ashwin1@gmail.com>
2020-05-21 12:53:12 -0400

    added test cases

--- login/tests/frontend/test_landing.py
+++ login/tests/frontend/test_landing.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from decouple import config
 
 
@@ -7,8 +8,7 @@ class LandingPageCase(LiveServerTestCase):
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.add_argument('--incognito')
-        path = config('WEB_DRIVER_PATH')
-        self.selenium = webdriver.Chrome(executable_path=path, chrome_options=options)
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
         super(LandingPageCase , self).setUp()
 
     def tearDown(self):
@@ -20,4 +20,3 @@ class LandingPageCase(LiveServerTestCase):
         selenium.get("http://127.0.0.1:8000/")
         landing_title = selenium.find_elements_by_tag_name('h1')
         assert len(landing_title) > 0
-
